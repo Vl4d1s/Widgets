1de29c1b98a8972d47301a3a702fc380806ef670
Vladis Markin <vladismarkin@gmail.com>
2020-09-27 19:50:48 +0300

    Helper Functions in Function Components

--- src/components/Accordion.js
+++ src/components/Accordion.js
@@ -1,7 +1,29 @@
 import React from "react";
 
 const Accordion = ({ items }) => {
-  return <h1>{items.length}</h1>;
+  const onTitleClick = (index) => {
+    console.log("Items clicked", index);
+  };
+
+  const renderedItems = items.map((item, index) => {
+    return (
+      <React.Fragment key={item.title}>
+        <div
+          className="title active"
+          onClick={() => {
+            onTitleClick(index);
+          }}
+        >
+          <i className="dropdown icon"></i>
+          {item.title}
+        </div>
+        <div className="content active"></div>
+        <p>{item.content}</p>
+      </React.Fragment>
+    );
+  });
+
+  return <div className="ui styled accordion">{renderedItems}</div>;
 };
 
 export default Accordion;
