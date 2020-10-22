cd79f278a5be0a086baf55db1982a3d374aa2520
Vladis Markin <vladismarkin@gmail.com>
2020-10-22 19:49:19 +0300

    Building a Link

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,18 +1,18 @@
-import React, { useState, useEffect } from "react";
-import axios from "axios";
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 
 const Search = () => {
-  const [term, setTerm] = useState("programming");
+  const [term, setTerm] = useState('programming');
   const [results, setResults] = useState([]);
 
   useEffect(() => {
     const search = async () => {
-      const { data } = await axios.get("https://en.wikipedia.org/w/api.php", {
+      const { data } = await axios.get('https://en.wikipedia.org/w/api.php', {
         params: {
-          action: "query",
-          list: "search",
-          origin: "*",
-          format: "json",
+          action: 'query',
+          list: 'search',
+          origin: '*',
+          format: 'json',
           srsearch: term,
         },
       });
@@ -33,16 +33,15 @@ const Search = () => {
         clearTimeout(timeoutId);
       };
     }
-  }, [term]);
+  }, [term, results.length]);
 
   const renderedResults = results.map((result) => {
+    console.log('I am ok.');
+
     return (
       <div key={result.pageid} className="item">
         <div className="right floated content">
-          <a
-            className="ui button"
-            href={`https://en.wikipedia.org?curid=${result.pageid}`}
-          >
+          <a className="ui button" href={`https://en.wikipedia.org?curid=${result.pageid}`}>
             Go
           </a>
         </div>
@@ -59,11 +58,7 @@ const Search = () => {
       <div className="ui form">
         <div className="field">
           <label>Enter Search Term</label>
-          <input
-            value={term}
-            onChange={(e) => setTerm(e.target.value)}
-            className="input"
-          />
+          <input value={term} onChange={(e) => setTerm(e.target.value)} className="input" />
         </div>
       </div>
       <div className="ui celled list">{renderedResults}</div>
